3b5b40a5c61bd4079f148d224ca727249c869eb0
bansodevaibhav032 <129043443+bansodevaibhav032@users.noreply.github.com>
2024-05-16 18:39:31 +0530

    some changes in runtime.txt and req.txt

--- vab/vab/settings.py
+++ vab/vab/settings.py
@@ -140,8 +140,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL= '/images/'
 
+
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
